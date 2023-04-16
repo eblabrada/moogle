6868feca62f3a10ab675e398a45a3255b668a06c
Eduardo Brito Labrada <eblabrada@gmail.com>
2023-04-16 19:48:15 -0400

    add snippet, edit distance and reorganize project

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -1,7 +1,57 @@
 ﻿namespace MoogleEngine;
 
+using System;
+using System.IO;
+using System.Text;
+
 public static class Utils
 {
+  public static int EditDistance(string a, string b)
+  {
+    int n = a.Length, m = b.Length;
+
+    int[,] dp = new int[2, m + 1];
+    for (int i = 0; i <= m; i++)
+    {
+      dp[0, i] = i;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+      int c = i & 1, o = 1 - (i & 1);
+      for (int j = 0; j <= m; j++)
+      {
+        if (j == 0)
+        {
+          dp[c, j] = i;
+        }
+        else
+        {
+          if (a[i - 1] == b[j - 1])
+          {
+            dp[c, j] = dp[o, j - 1];
+          }
+          else
+          {
+            dp[c, j] = Math.Min(dp[o, j], Math.Min(dp[o, j - 1], dp[c, j - 1])) + 1;
+          }
+        }
+      }
+    }
+
+    return dp[n & 1, m];
+  }
+
+  public static bool AreSimilar(string a, string b)
+  {
+    a = a.ToLower(); b = b.ToLower();
+    if (b.Length > 1 && EditDistance(a, b) <= 1)
+    {
+      return true;
+    }
+    return false;
+  }
+
   public static string Tokenizer(string word)
   {
     string res = "";
@@ -13,6 +63,22 @@ public static class Utils
       }
     }
     return res.ToLower();
+  }
+
+  public static List<string> NormalizeText(string text)
+  {
+    char[] splitters = { ' ', ',', '.', ':', ';', '\t', '\n' };
+    string[] words = text.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+    List<string> res = new List<string>();
+    for (int i = 0; i < words.Length; i++)
+    {
+      string s = Tokenizer(words[i]);
+      if (s.Length > 0)
+      {
+        res.Add(s);
+      }
+    }
+    return res;
   }
 
   public static double Norm(Dictionary<string, double> vec)
@@ -35,6 +101,7 @@ public class TFIDFAnalyzer
   public Dictionary<string, List<int>> vocabulary = new Dictionary<string, List<int>>();
   public string path = "";
   public List<string> documents = new List<string>();
+  public List<List<string>> fdocuments = new List<List<string>>();
   public Dictionary<int, string> documentTitle = new Dictionary<int, string>();
   public int numberOfDocuments = 0;
 
@@ -48,7 +115,9 @@ public class TFIDFAnalyzer
       int currentIndex = 0;
       foreach (var dir in directories)
       {
-        this.documents.Add(File.ReadAllText(dir));
+        string text = File.ReadAllText(dir, Encoding.UTF8);
+        this.documents.Add(text);
+        this.fdocuments.Add(Utils.NormalizeText(text));
         this.documentTitle[currentIndex] = Path.GetFileNameWithoutExtension(dir);
         currentIndex++;
       }
@@ -58,7 +127,7 @@ public class TFIDFAnalyzer
       for (int i = 0; i < numberOfDocuments; i++)
       {
         TF.Add(new Dictionary<string, double>());
-        ProcessDocuments(documents[i], i);
+        ProcessDocuments(fdocuments[i], i);
       }
 
       foreach (string term in vocabulary.Keys)
@@ -80,17 +149,11 @@ public class TFIDFAnalyzer
       Console.WriteLine($"The process failed: {e.ToString()}");
     }
   }
-  private void ProcessDocuments(string doc, int index)
+  private void ProcessDocuments(List<string> doc, int index)
   {
-    char[] splitters = { ' ', ',', '.', ':', ';', '\t', '\n' };
-    string[] words = doc.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
-    for (int i = 0; i < words.Length; i++)
+    for (int i = 0; i < doc.Count(); i++)
     {
-      string word = Utils.Tokenizer(words[i]);
-      if (word.Length == 0)
-      {
-        continue;
-      }
+      string word = doc[i];
 
       if (!TF[index].ContainsKey(word)) TF[index].Add(word, 0.0);
       if (!vocabulary.ContainsKey(word)) vocabulary.Add(word, new List<int>());
@@ -105,7 +168,7 @@ public class TFIDFAnalyzer
 
     foreach (string term in TF[index].Keys)
     {
-      TF[index][term] /= (double)words.Length;
+      TF[index][term] /= (double)doc.Count();
     }
   }
 
@@ -136,38 +199,86 @@ public class TFIDFAnalyzer
   }
 }
 
-public class Moogle
+public static class SearchEngine
 {
-  public static TFIDFAnalyzer AllDocuments = new TFIDFAnalyzer("../Content");
+  public static TFIDFAnalyzer allDocuments = new TFIDFAnalyzer("../Content");
 
-  public static List<(double, int)> findItems(string query)
+  private static SearchItem CalculateSnippet(SearchItem item, string query, int len = 200)
+  {
+    char[] splitters = { ' ', ',', '.', ':', ';', '\t', '\n' };
+    string[] text = item.Snippet.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+
+    string str = ""; int p = 0;
+    while (p < Math.Min(len / 4, text.Length))
+    {
+      str += text[p] + " ";
+      p++;
+    }
+
+    string snippet = str + " (...) ";
+
+    string[] queryText = query.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+
+    int cnt = 0;
+    Queue<string> todo = new Queue<string>();
+    while (p < text.Length && todo.Count() < len / 2)
+    {
+      todo.Enqueue(text[p]);
+      for (int i = 0; i < queryText.Length; i++)
+      {
+        if (Utils.AreSimilar(queryText[i], text[p]))
+        {
+          cnt++;
+        }
+      }
+      p++;
+    }
+
+    int position = p, maxCnt = cnt;
+    for (int i = p; i < text.Length; i++)
+    {
+      todo.Enqueue(text[p]);
+
+      bool ok = false;
+      for (int j = 0; j < queryText.Length; j++)
+      {
+        if (Utils.AreSimilar(queryText[j], text[i]))
+        {
+          cnt++;
+        }
+
+        if (Utils.AreSimilar(queryText[j], todo.Peek()))
+        {
+          ok = true;
+        }
+      }
+
+      todo.Dequeue();
+
+      if (ok) cnt--;
+
+      if (cnt > maxCnt)
+      {
+        position = i;
+        maxCnt = cnt;
+      }
+    }
+
+    for (int i = Math.Max(0, position - len / 2); i <= Math.Min(position, text.Length - 1); i++)
+    {
+      snippet = snippet + text[i] + " ";
+    }
+
+    return new SearchItem(item.Title, snippet, item.Score);
+  }
+
+  public static List<SearchItem> FindItems(string query)
   {
     Dictionary<string, double> QTF = new Dictionary<string, double>();
-
-    List<string> forbidden = new List<string>();
-    List<string> needed = new List<string>();
 
     string[] words = query.Split();
     for (int i = 0; i < words.Length; i++)
     {
-      int importance = 1;
-      if (words[i][0] == '!')
-      {
-        importance = 0;
-        forbidden.Add(Utils.Tokenizer(words[i].Substring(1)));
-      }
-
-      if (words[i][0] == '^')
-      {
-        needed.Add(Utils.Tokenizer(words[i].Substring(1)));
-      }
-
-      int j = 0;
-      while (j < words[i].Length && words[i][j] == '*')
-      {
-        j++; importance++;
-      }
-
       string word = Utils.Tokenizer(words[i]);
       if (word.Length == 0)
       {
@@ -175,7 +286,7 @@ public class Moogle
       }
 
       if (!QTF.ContainsKey(word)) QTF.Add(word, 0.0);
-      QTF[word] = importance;
+      QTF[word] = 1;
     }
 
     foreach (string term in QTF.Keys)
@@ -184,31 +295,11 @@ public class Moogle
     }
 
     List<(double, int)> items = new List<(double, int)>();
-    for (int i = 0; i < AllDocuments.numberOfDocuments; i++)
+    for (int i = 0; i < allDocuments.numberOfDocuments; i++)
     {
-      double similarity = AllDocuments.ComputeRelevance(ref QTF, i);
+      double similarity = allDocuments.ComputeRelevance(ref QTF, i);
 
-      bool ok = true;
-
-      // ignore forbidden words
-      foreach (var term in forbidden)
-      {
-        if (AllDocuments.TF[i].ContainsKey(term))
-        {
-          ok = false;
-        }
-      }
-
-      // consider needed words
-      foreach (var term in needed)
-      {
-        if (AllDocuments.TF[i].ContainsKey(term) == false)
-        {
-          ok = false;
-        }
-      }
-
-      if (similarity == 0 || !ok) continue;
+      if (similarity == 0) continue;
 
       Console.WriteLine($"{i} {similarity}");
       items.Add((similarity, i));
@@ -216,19 +307,25 @@ public class Moogle
 
     items.Sort(); items.Reverse();
 
-    return items;
-  }
-
-  public static SearchResult Query(string query)
-  {
-    var res = findItems(query);
-
-    SearchItem[] items = new SearchItem[res.Count()];
-    for (int i = 0; i < res.Count(); i++)
+    List<SearchItem> res = new List<SearchItem>();
+    for (int i = 0; i < items.Count(); i++)
     {
-      items[i] = new SearchItem(AllDocuments.documentTitle[res[i].Item2], "Falta por implementar", (float)res[i].Item1);
+      string title = allDocuments.documentTitle[items[i].Item2];
+      string snippet = allDocuments.documents[items[i].Item2];
+      res.Add(CalculateSnippet(new SearchItem(title, snippet, (float)items[i].Item1), query));
     }
 
+    return res;
+  }
+}
+
+public static class Moogle
+{
+  public static SearchResult Query(string query)
+  {
+    List<SearchItem> res = new List<SearchItem>(SearchEngine.FindItems(query));
+
+    SearchItem[] items = res.ToArray();
     return new SearchResult(items, query);
   }
 }
