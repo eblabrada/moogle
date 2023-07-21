d5ab25ef5435efb033ac86f99ab320d9edcc4ebe
Eduardo Brito Labrada <eblabrada@gmail.com>
2023-07-21 14:52:58 -0400

    update script

--- Script/proyecto.sh
+++ Script/proyecto.sh
@@ -33,6 +33,12 @@ clean() {
   cd Presentacion
   rm -f presentation.aux presentation.fls presentation.log presentation.nav presentation.synctex.gz presentation.snm presentation.toc presentation.fdb_latexmk
   cd ..
+  cd MoogleServer
+  rm -r bin obj
+  cd ..
+  cd MoogleEngine
+  rm -r bin obj
+  cd ..
 }
 
 all_docs() {
